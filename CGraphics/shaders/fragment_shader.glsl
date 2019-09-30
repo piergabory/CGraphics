f34f7a6fa000c5cce841169ab704e4158ff6a936
Pierre Gabory <piergabory@gmail.com>
2019-09-30 12:12:15 -0500

    Normal Matrix

--- CGraphics/shaders/fragment_shader.glsl
+++ CGraphics/shaders/fragment_shader.glsl
@@ -1,7 +1,10 @@
 #version 400
-in vec3 vertex_colour;
+
+in vec3 vertex_position;
+in vec3 vertex_normal;
+
 out vec3 fragment_colour;
 
 void main() {
-    fragment_colour = vertex_colour;
+    fragment_colour = vertex_normal;
 }
--- CGraphics/shaders/vertex_shader.glsl
+++ CGraphics/shaders/vertex_shader.glsl
@@ -6,9 +6,11 @@ uniform mat4 model_view_projection_matrix_uniform;
 uniform mat4 model_view_matrix_uniform;
 uniform mat4 normal_matrix_uniform;
 
-out vec3 vertex_colour;
+out vec3 vertex_position;
+out vec3 vertex_normal;
 
 void main() {
-    vertex_colour = vertex_colour_attribute;
+    vertex_position = (model_view_matrix_uniform * vec4(vertex_position_attribute, 1)).xyz;
+    vertex_normal = (normal_matrix_uniform * vec4(vertex_position_attribute, 1)).xyz;
     gl_Position = model_view_projection_matrix_uniform * vec4(vertex_position_attribute, 1);
 }
